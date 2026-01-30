9a0529d9253e6692bf1f9a92c46640566ea90fd1
code_urge <bansalyash316@gmail.com>
2026-01-31 05:08:33 +0530

    Time: 19 ms (8.03%), Space: 21.9 MB (12.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int xor1 = 0;
-        int xor2 = 0;
-        for(int i = 0;i<nums.size();i++) {
-            xor1 ^= (i);
-            xor2 ^= nums[i];
+        
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0;i<nums.size()-1;i++) {
+            if(nums[i+1]-nums[i] > 1) {
+                return nums[i+1]-1;
+            }
         }
 
-        xor1 ^= nums.size();
+        if(nums[0] != 0) return 0;
 
-        return xor1^xor2;
+        return nums.size();
 
     }
 };
